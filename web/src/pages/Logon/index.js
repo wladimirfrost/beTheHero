3fb856fd5c34356be2dea90644a1683c4af1281f
wladimir <wladimir.code@gmail.com>
2020-03-26 00:57:23 -0300

    feat: Api connection (axios), properties state, history navigation

--- web/src/pages/Logon/index.js
+++ web/src/pages/Logon/index.js
@@ -1,22 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
 
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 import herosImg from '../../assets/heroes.png';
 
 export default function Logon() {
+  const [id, setId] = useState('');
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('/sessions', { id });
+      const { name } = response.data;
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', name);
+
+      history.push('/profile');
+    } catch (err) {
+      alert('Login Failed!');
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
 
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Log in</h1>
 
-          <input placeholder="Your ID" />
+          <input
+            placeholder="Your ID"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
           <button className="button" type="submit">
             Log in
           </button>
--- web/src/pages/Register/index.js
+++ web/src/pages/Register/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
 
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 
 export default function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+  const [city, setCity] = useState('');
+  const [uf, setUf] = useState('');
+
+  const history = useHistory();
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    };
+
+    try {
+      const response = await api.post('/ongs', data);
+
+      const { id } = response.data;
+
+      alert(`Your ID ${id}`);
+
+      history.push('/');
+    } catch (err) {
+      alert('Register Failed!');
+    }
+  }
+
   return (
     <div className="register-container">
       <div className="content">
@@ -24,14 +57,36 @@ export default function Register() {
           </Link>
         </section>
 
-        <form>
-          <input placeholder="ONG Name" />
-          <input type="email" placeholder="E-mail" />
-          <input placeholder="WhatsApp" />
+        <form onSubmit={handleRegister}>
+          <input
+            placeholder="ONG Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="WhatsApp"
+            value={whatsapp}
+            onChange={(e) => setWhatsapp(e.target.value)}
+          />
 
           <div className="input-group">
-            <input placeholder="City" />
-            <input placeholder="UF" style={{ width: 80 }} />
+            <input
+              placeholder="City"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <input
+              placeholder="UF"
+              value={uf}
+              onChange={(e) => setUf(e.target.value)}
+              style={{ width: 80 }}
+            />
           </div>
 
           <button className="button" type="submit">
