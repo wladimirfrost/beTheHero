7cb5191e31bbb707d4ae1a817d5078d39defb80f
Wladimir <wladimirgrf@gmail.com>
2020-03-26 00:57:23 -0300

    feat: Api connection (axios), properties state, history navigation

--- web/src/pages/NewIncident/index.js
+++ web/src/pages/NewIncident/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
 
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 
 export default function NewIncident() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const history = useHistory();
+
+  const ongId = localStorage.getItem('ongId');
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId,
+        },
+      });
+
+      history.push('/profile');
+    } catch (err) {
+      alert('Register new incident has failed!');
+    }
+  }
+
   return (
     <div className="new-incident-container">
       <div className="content">
@@ -21,10 +52,22 @@ export default function NewIncident() {
           </Link>
         </section>
 
-        <form>
-          <input placeholder="Incident Title" />
-          <textarea placeholder="Description" />
-          <input placeholder="Amount" />
+        <form onSubmit={handleNewIncident}>
+          <input
+            placeholder="Incident Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input
+            placeholder="Amount"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
 
           <button className="button" type="submit">
             Save
