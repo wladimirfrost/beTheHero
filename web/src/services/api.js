44042235422df8bbc6d5f8b5d14b40021709d549
Wladimir <wladimir.code@gmail.com>
2020-03-28 15:18:29 -0300

    feat: Change api connection for Gitpod

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://3333-f84b3925-3e55-4ba8-b1a0-52da2c85ee56.ws-us02.gitpod.io',
 });
 
 export default api;
