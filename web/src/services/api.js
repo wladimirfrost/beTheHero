2735231ab687b2e037008c0071c3c149e399669d
Wladimir <wladimir.code@gmail.com>
2020-04-06 21:52:22 -0300

    add: Update api baseURL

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://3333-f84b3925-3e55-4ba8-b1a0-52da2c85ee56.ws-us02.gitpod.io',
+  baseURL: 'http://localhost:3333',
 });
 
 export default api;
